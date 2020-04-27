5c7b7b87d9b51a6e78dc1772f652209ce34be712
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-04-27 20:56:29 +0200

    Add project files.

---
+++ WebHookAPIZBot/Controllers/WebhookReciveverController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHookAPIZBot.Controllers
+{
+    [ApiController]
+    [Route("~/")]
+    public class WebhookReciveverController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        public WebhookReciveverController(ILogger<WebhookReciveverController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("Created");
+            
+        }
+
+        [HttpPost("recieve")]
+        public void Recieve([FromBody]Github response)
+        {
+
+            if (response.Sender.Name == "ZebastianBjorkqvist")
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "/home/zebbe/sambashare/ZbotGit/ClonePublishRunZBot.sh";
+                psi.UseShellExecute = false;
+                psi.RedirectStandardOutput = true;
+
+                Process p = Process.Start(psi);
+                string strOutput = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                Console.WriteLine(strOutput);
+            }
+        }
+    }
+}
---
+++ WebHookAPIZBot/Models/GitHubResponse.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WebHookAPIZBot
+{
+    public partial class Github
+    {
+        [JsonProperty("ref")]
+        public string Ref { get; set; }
+
+        [JsonProperty("pusher")]
+        public Pusher Pusher { get; set; }
+
+        [JsonProperty("repository")]
+        public Repository Repository { get; set; }
+
+        [JsonProperty("sender")]
+        public Sender Sender { get; set; }
+    }
+
+    public partial class Pusher
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+
+    public partial class Repository
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("full_name")]
+        public string FullName { get; set; }
+
+        [JsonProperty("private")]
+        public bool Private { get; set; }
+
+        [JsonProperty("owner")]
+        public Sender Owner { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri HtmlUrl { get; set; }
+
+        [JsonProperty("description")]
+        public object Description { get; set; }
+
+        [JsonProperty("fork")]
+        public bool Fork { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("created_at")]
+        public long CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonProperty("pushed_at")]
+        public long PushedAt { get; set; }
+    }
+
+    public partial class Sender
+    {
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
+        public string Email { get; set; }
+
+    }
+}
